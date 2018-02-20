c18aeeb5b81a87dba424670c1a8c20a931ea3032
gunee7 <gunee7@naver.com>
2018-02-20 17:54:10 +0900

    a

--- coffeeproject_succeslatte/st1drawermenu/src/main/java/com/example/st1drawermenu/SubuMenu/QrcodeActivity.java
+++ coffeeproject_succeslatte/st1drawermenu/src/main/java/com/example/st1drawermenu/SubuMenu/QrcodeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.st1drawermenu.MainActivity;
@@ -12,6 +13,8 @@ import com.example.st1drawermenu.R;
 
 public class QrcodeActivity extends AppCompatActivity {
 
+
+
     private ImageView btn_xmark2;
 
     @Override
@@ -19,13 +22,15 @@ public class QrcodeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
 
-        btn_xmark2 = findViewById( R.id.btn_xmark2 );
+     btn_xmark2 = findViewById( R.id.btn_xmark2          );
+
         btn_xmark2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent( /* context */ QrcodeActivity.this  ,  /* class 이름 */ MainActivity.class);
-                startActivity ( i );
+                finish();
             }
         });
     }
+
+
 }
